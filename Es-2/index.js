d8ff15c2bf38e345ec9854837d6e208744c9b77b
Ignazioconti <108429510+Ignazioconti@users.noreply.github.com>
2023-04-02 20:03:10 +0200

    Add files via upload

---
+++ Es-2/index.js
@@ -0,0 +1,10 @@
+const repl = require("node:repl");
+const crypto = require("node:crypto");
+
+repl
+  .start("> ")
+  .on("close", () => {
+    console.log(crypto);
+    console.log(crypto.randomUUID());
+  })
+  .close();
